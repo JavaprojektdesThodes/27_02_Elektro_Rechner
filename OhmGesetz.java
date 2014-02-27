563806fed1d925a21cfcc4405841e69c1b9c7b03
marcelomarshmallow <marcel.kolzau@gmx.de>
2014-02-27 07:27:28 +0100

    Create OhmGesetz.java

---
+++ OhmGesetz.java
@@ -0,0 +1,30 @@
+import java.awt.*;
+import java.awt.event.*;
+
+public class OhmGesetz extends Frame
+{
+  
+  //----------------------------------------------------------------------------
+  // Schriftarten definieren
+  //----------------------------------------------------------------------------
+  Font Ueberschrift = new Font("Arial", Font.BOLD, 16);
+  Font Normal = new Font("Arial", Font.PLAIN, 12);
+  //----------------------------------------------------------------------------
+  
+  public OhmGesetz()
+  {
+    
+  }    
+  
+  public void Text(Graphics g)
+  {
+    g.setColor(Color.black);
+    g.fillRoundRect(49,69,502,512,30,30);
+    g.setColor(Color.white);
+    g.fillRoundRect(50,70,500,510,30,30);
+    g.setColor(Color.black); 
+    
+    g.setFont(Ueberschrift);
+    g.drawString("Ohm'sches Gesetz",230,100);
+  }
+}
