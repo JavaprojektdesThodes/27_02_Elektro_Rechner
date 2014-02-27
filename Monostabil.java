16ab943be15ea7961be25454a468341ef3c3318b
marcelomarshmallow <marcel.kolzau@gmx.de>
2014-02-27 07:29:05 +0100

    Create Monostabil.java

---
+++ Monostabil.java
@@ -0,0 +1,30 @@
+import java.awt.*;
+import java.awt.event.*;
+
+public class Monostabil extends Frame
+{
+  
+  //----------------------------------------------------------------------------
+  // Schriftarten definieren
+  //----------------------------------------------------------------------------
+  Font Ueberschrift = new Font("Arial", Font.BOLD, 16);
+  Font Normal = new Font("Arial", Font.PLAIN, 12);
+  //----------------------------------------------------------------------------
+  
+  public Monostabil()
+  {
+    
+  }    
+  
+  public void Text(Graphics g)
+  {
+    g.setColor(Color.black);
+    g.fillRoundRect(49,69,502,512,30,30);
+    g.setColor(Color.white);
+    g.fillRoundRect(50,70,500,510,30,30);
+    g.setColor(Color.black); 
+    
+    g.setFont(Ueberschrift);
+    g.drawString("Monostabil",250,100);
+  }
+}
